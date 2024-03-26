38965b09516fa6e175fd8610250d4b632dff22d2
Rextens <jskudrys@gmail.com>
2024-03-26 16:33:59 +0100

    Client can now connect to the server via TCP

--- Source/TCPTutorial/Private/TCPController.cpp
+++ Source/TCPTutorial/Private/TCPController.cpp
@@ -34,14 +34,49 @@ void ATCPController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if(GetNetMode() == NM_Client)
+	{
+		TArray<uint8> Bytes;
+
+		if(ClientSocket)
+		{
+			if(ClientSocket->GetConnectionState() == SCS_Connected)
+			{
+				uint32 PendingData = 0;
+				if(ClientSocket->HasPendingData(PendingData))
+				{
+					Bytes.SetNum(PendingData);
+					int32 BytesRead = 0;
+					if(ClientSocket->Recv(Bytes.GetData(), PendingData, BytesRead))
+					{
+						FString Message = "";
+						FMemoryReader MemoryReader(Bytes);
+
+						MemoryReader << Message;
+
+						UKismetSystemLibrary::PrintString(GetWorld(), Message, true, true, FLinearColor::Red, 50.f);
+					}
+				}
+			}
+		}
+	}
 }
 
 bool ATCPController::ClientConnected(FSocket* Socket, const FIPv4Endpoint& FiPv4Endpoint)
 {
-	AsyncTask(ENamedThreads::GameThread, [&]()
+	UKismetSystemLibrary::PrintString(GetWorld(), "CLIENT CONNECTED!");
+
+	TArray<uint8> Bytes;
+	FMemoryWriter MemoryWriter(Bytes);
+
+	FString Message = "Industrial Black Metal is great for studying!";
+	MemoryWriter << Message;
+
+	int32 BytesSent = 0;
+	if(!Socket->Send(Bytes.GetData(), Bytes.Num(), BytesSent))
 	{
-		UKismetSystemLibrary::PrintString(GetWorld(), "CLIENT CONNECTED!");
-	});
+		UKismetSystemLibrary::PrintString(GetWorld(), "Unable to send data");
+	}
 
 	return true;
 }
@@ -50,7 +85,7 @@ void ATCPController::Connect(FString ServerIp)
 {
 	FIPv4Address IpAddress;
 	FIPv4Address::Parse(ServerIp, IpAddress);
-	FIPv4Endpoint Endpoint(IpAddress, 2213);
+	FIPv4Endpoint Endpoint(IpAddress, (uint16)2213);
 
 	ClientSocket = FTcpSocketBuilder(TEXT("Client Socket")).AsReusable().WithReceiveBufferSize(BufferSize).AsNonBlocking();
 	ISocketSubsystem* SocketSubsystem = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM);
@@ -60,4 +95,7 @@ void ATCPController::Connect(FString ServerIp)
 	Addr->SetPort(Endpoint.Port);
 
 	ClientSocket->Connect(*Addr);
+	ClientSocket->Bind(*Addr);
+	int32 NewSize = 0;
+	ClientSocket->SetReceiveBufferSize(BufferSize, NewSize);
 }
