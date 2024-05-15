1af285a9350d6de32a3b93ee12a07124f71c7abd
Rextens <jskudrys@gmail.com>
2024-05-15 23:01:00 +0200

    Added example struct

---
+++ Source/TCPTutorial/Public/ExampleStruct.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ExampleStruct.generated.h"
+
+USTRUCT()
+struct FItem
+{
+	GENERATED_BODY()
+	
+	int32 ItemId;
+
+	int32 Quantity;
+
+	FString Description;
+};
+
+inline FArchive& operator<<(FArchive& Ar, FItem& Data)
+{
+	Ar << Data.ItemId;
+	Ar << Data.Quantity;
+	Ar << Data.Description;
+
+	return Ar;
+}
+
+USTRUCT()
+struct FExampleInventory
+{
+	GENERATED_BODY()
+
+	TMap<int32, FItem> InventorySlots;
+};
+
+inline FArchive& operator<<(FArchive& Ar, FExampleInventory& Data)
+{
+	Ar << Data.InventorySlots;
+
+	return Ar;
+}
